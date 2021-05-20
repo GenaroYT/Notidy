d23a17dfd12218a8e9ba6595fb0d45d117af556a
GenaroYT <81038333+GenaroYT@users.noreply.github.com>
2021-05-19 22:17:21 -0300

    Base folders and files

---
+++ rss_feed/rss_feed_ny/app.js
@@ -0,0 +1,27 @@
+  let url = 'https://rss.nytimes.com/services/xml/rss/nyt/World.xml';
+  const textarea = document.querySelector('#feed-textarea > ul');
+
+  const date = new Date();
+  document.querySelector('#date').innerHTML = date.toDateString();
+    
+    feednami.load(url)
+    .then(feed => {
+      textarea.value = ''
+      console.log(feed);
+      for(let entry of feed.entries){
+          //create a list element
+          let li = document.createElement('li');
+          //add HTML content to list items
+          li.innerHTML = `<h4><strong><a href="${entry.link}">${entry.title}</a></strong></h4>`;
+          //append HTML content to list 
+          textarea.appendChild(li);
+      }
+    });
+
+
+//Using feednami to fetch RSS feeds
+//https://toolkit.sekando.com/docs/en/feednami
+
+//Feeds from BBC News
+//https://www.bbc.com/news/10628494#userss
+  
